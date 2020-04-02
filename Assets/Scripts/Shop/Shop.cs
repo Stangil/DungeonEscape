98659dfe2700773966c8c5d26eba205125345de2
Stangil <stangil44@gmail.com>
2020-04-02 14:51:20 -0400

    Added shop enabling

---
+++ Assets/Scripts/Shop/Shop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public GameObject shopPanel;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Activate shop");
+            shopPanel.SetActive(true);
+        }
+    }
+}

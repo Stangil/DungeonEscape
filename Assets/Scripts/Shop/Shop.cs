ed90f735d34ec8016900d6d46a7794edc4dbfe93
Stangil <stangil44@gmail.com>
2020-04-02 18:46:22 -0400

    Enable item selection in shop

--- Assets/Scripts/Shop/Shop.cs
+++ Assets/Scripts/Shop/Shop.cs
@@ -9,8 +9,42 @@ public class Shop : MonoBehaviour
     {
         if(other.tag == "Player")
         {
-            Debug.Log("Activate shop");
+            Player player = other.GetComponent<Player>();
+            if(player != null)
+            {
+                UIManager.UIinstance.OpenShop(player.diamonds);
+            }
+            //Debug.Log("Activate shop");
             shopPanel.SetActive(true);
+
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            shopPanel.SetActive(false);
+        }
+    }
+
+    public void SelectItem(int item)
+    {
+        
+        Debug.Log("Button clicked " + item);
+
+        switch(item)
+        {
+            case 0://flame sword
+                UIManager.UIinstance.UpdateShopSelection(55);
+                break;
+            case 1://Boots of flight
+                UIManager.UIinstance.UpdateShopSelection(-56);
+                break;
+            case 2://Key to Castle
+                UIManager.UIinstance.UpdateShopSelection(-164);
+                break;
+        }
+
     }
 }
---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;   
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+    public static UIManager UIinstance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                Debug.LogError("UIManager is null");
+            }
+            return _instance;
+        }
+    }
+
+    public Text playerGemCountText;
+    public Image selectionImage;
+    public void OpenShop(int gemCount)
+    {
+        playerGemCountText.text = "" + gemCount + "G"; 
+    }
+
+    public void UpdateShopSelection(int yPos)
+    {
+        selectionImage.rectTransform.anchoredPosition = new Vector2(selectionImage.rectTransform.anchoredPosition.x, yPos);
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+}

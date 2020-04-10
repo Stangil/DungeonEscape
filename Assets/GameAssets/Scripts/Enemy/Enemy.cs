a1c47eab96bcc28273575547a4f787097f7d8696
Stangil <stangil44@gmail.com>
2020-04-10 16:25:54 -0400

    Started new scene

---
+++ Assets/GameAssets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    protected int health;
+    [SerializeField]
+    protected float speed;
+    [SerializeField]
+    protected int gems;
+    public GameObject diamondPrefab;
+    [SerializeField]
+    protected Transform pointA, pointB;//waypoints
+    [SerializeField]
+    protected float waypointDistance = 0.1f;
+    [SerializeField]
+    protected float inCombatDistance = 2.0f;
+    protected bool direction = false;
+    protected Vector3 currentTarget;
+    protected Animator animator;
+    protected SpriteRenderer sprite;
+
+    protected bool isHit = false;
+    [SerializeField]
+    protected Player player;
+    protected bool isDead = false;
+    public virtual int Health { get; set; }
+    //var to store player
+    public virtual void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle") && animator.GetBool("InCombat") == false)
+        {
+            return;
+        }
+        if(isDead == false)
+        {
+            Movement();
+        }
+        
+    }
+    private void Start()
+    {
+        Init();
+    }
+    public virtual void Init()
+    {
+        animator = GetComponentInChildren<Animator>();
+        currentTarget = pointA.position;
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    public virtual void Movement()
+    {
+        InCombatLogic();
+
+        if (Vector3.Distance(transform.position, pointA.position) < waypointDistance)
+        {
+            currentTarget = pointB.position;
+            animator.SetTrigger("Idle");
+            direction = true;
+        }
+        else if (Vector3.Distance(transform.position, pointB.position) < waypointDistance)
+        {
+            currentTarget = pointA.position;
+            animator.SetTrigger("Idle");
+            direction = false;
+        }
+
+        if (isHit == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+        }
+        FlipSprite(direction);
+    }
+
+    private void InCombatLogic()
+    {
+        //distance to player
+        float distance = Vector3.Distance(transform.localPosition, player.transform.localPosition);
+        //direction to player
+        Vector3 playerDirection = player.transform.localPosition - transform.localPosition;
+
+        if (distance > inCombatDistance)
+        {
+            isHit = false;
+            animator.SetBool("InCombat", false);
+        }
+
+        if (animator.GetBool("InCombat") == true)
+        {
+            if (playerDirection.x > 0)
+            {
+                direction = true;
+            }
+            else if (playerDirection.x < 0)
+            {
+                direction = false;
+            }
+        }
+        else
+        {
+            if(currentTarget == pointA.position)
+            {
+                direction = false;
+            }
+            else
+            {
+                direction = true;
+            }
+        }
+    }
+
+    protected void FlipSprite(bool move)
+    {
+        if (move)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (!move)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+    public virtual void Damage()
+    {
+        if (isDead)
+        {
+            return;
+        }
+        Health--;
+        animator.SetTrigger("Hit");
+        isHit = true;
+        animator.SetBool("InCombat", true);
+        if (Health < 1)
+        {
+            animator.SetTrigger("Death");
+            isDead = true;
+            GameObject diamond = Instantiate(diamondPrefab, transform.position - new Vector3(0.5f, 0, 0), Quaternion.identity) as GameObject;
+            diamond.GetComponent<Diamond>().gems = gems;
+        }
+    } 
+
+    public virtual void Attack(){}
+}
